95e60e86359848dca597e9e4dd4ba75622746611
WalidAkash <65769832+WalidAkash@users.noreply.github.com>
2023-05-23 09:25:40 +0600

    Update fifo_mod.sv using case statement instead of if-else condition

--- fifo_mod.sv
+++ fifo_mod.sv
@@ -20,6 +20,8 @@ logic [DataWidth-1:0] fifo_memory[Depth-1:0];
 logic [$clog2(Depth):0] wr_ptr, rd_ptr;
 logic full, empty;
 logic din_hs, dout_hs;
+logic [2:0] sel;
+integer count = 0;
 
 assign full = (wr_ptr[$clog2(Depth)] ^ rd_ptr[$clog2(Depth)]) && (wr_ptr[$clog2(Depth)-1:0] == rd_ptr[$clog2(Depth)-1:0]);
 
@@ -31,48 +33,47 @@ assign din_rdy_o= !full;
 assign dout_val_o = !empty;
 
 
-//handhsakes
-//input side handshake
-assign din_hs = din_val_i && din_rdy_o;  
-//output side handshake   
-assign dout_hs = dout_val_o && dout_rdy_i;
-//input/output both side handshake
-assign din_out_hs = din_val_i && din_rdy_o && dout_val_o && dout_rdy_i;
-//No handshake
-assign no_hs = din_val_i || din_rdy_o || dout_val_o || dout_rdy_i;
-
-
-
 always_ff@(posedge clk_i, negedge arst_ni) begin
-    if (!arst_ni) begin
-        wr_ptr <= '0;
-        for (int i = 1; i<Depth; i++) begin
-            fifo_memory[i] <= '0;
-        end
-        rd_ptr <= '0;
-    end
-    //input side handshake operation
-    else if (din_hs) begin
-        fifo_memory[wr_ptr] <= din_i;
-        wr_ptr <= wr_ptr + 1'b1;
-    end
-    //output side handshake operation
-    else if (dout_hs) begin
-        rd_ptr <= rd_ptr + 1'b1;
-    end
-    //input/output both side handshake operation
-    else if (din_out_hs) begin
-        fifo_memory[wr_ptr] <= din_i;
-        wr_ptr <= wr_ptr + 1'b1;
-        rd_ptr <= rd_ptr + 1'b1;
-    end
-    //No handshake operation
-    else if (no_hs) begin
-        wr_ptr <= wr_ptr;
-        rd_ptr <= rd_ptr;
-    end
+    case (sel[2:0])
+        3'b01:
+            begin
+                //count  <= (count - 1);
+                wr_ptr <= wr_ptr;
+                if ((rd_ptr+1)<Depth)
+                    rd_ptr <= rd_ptr+1;
+                else
+                    rd_ptr <= '0;
+            end
+        3'b10:
+            begin
+                //count  <= (count +1);
+                fifo_memory [wr_ptr] <= din_i;                    
+                if ((wr_ptr+1)<Depth)
+                    wr_ptr <= wr_ptr+1;
+                else
+                    wr_ptr <= '0;
+                rd_ptr <= rd_ptr;
+            end
+        3'b11: begin
+            //count  <= count;
+            fifo_memory [wr_ptr] <= din_i;
+            if ((wr_ptr+1)<Depth)
+                wr_ptr <= wr_ptr+1;
+            else
+                wr_ptr <= '0;
+            if ((rd_ptr+1)<Depth)
+                rd_ptr <= rd_ptr+1;
+            else
+                rd_ptr <= '0;
+            end
+        default:
+            begin
+                //count  <= count;
+                wr_ptr <= wr_ptr;
+                rd_ptr <= rd_ptr;
+            end
+    endcase
 end
-
 //fall through reads, combinational reads
 assign dout_o = fifo_memory[rd_ptr];
 
