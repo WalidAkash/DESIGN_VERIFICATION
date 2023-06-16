e22837291110092bc886562121c032d028772c1e
WalidAkash <walidakash070@gmail.com>
2023-06-16 12:08:10 +0600

    ..

--- risc_v/verif/tb_execute_stage.sv
+++ risc_v/verif/tb_execute_stage.sv
@@ -1,22 +1,38 @@
 // Designer : Walid Akash
 // Company : DSi
-// Description : Execute stage pipeline register testbench
+// Description : Execute stage pipeline logicister testbench
 
-module tb_execute_stage();
+module tb_execute_stage;
 
-execute_stage 
-execute_stage_dut (
-  .clk (clk ),
-  .regwriteE (regwriteE ),
-  .resultsrcE (resultsrcE ),
-  .memwriteE (memwriteE ),
-  .aluresultE (aluresultE ),
-  .Rd2E (Rd2E ),
-  .RdE (RdE ),
-  .regwriteM (regwriteM ),
-  .resultsrcM (resultsrcM ),
-  .memwriteM (memwriteM ),
-  .aluresultM (aluresultM ),
-  .Rd2M (Rd2M ),
-  .RdM  ( RdM)
-);
+logic r_clk;
+logic r_logicwriteE;
+logic r_resultsrcE;
+logic r_memwriteE;
+logic [DPW-1:0] r_aluresultE;
+logic [DPW-1:0] r_Rd2E;
+logic [4:0] r_RdE;
+logic r_logicwriteM;
+logic r_resultsrcM;
+logic r_memwriteM;
+logic [DPW-1:0] r_aluresultM;
+logic [DPW-1:0] r_Rd2M;
+logic [4:0] r_RdM;
+
+
+  execute_stage execute_stage_dut (
+      .clk(clk),
+      .logicwriteE(logicwriteE),
+      .resultsrcE(resultsrcE),
+      .memwriteE(memwriteE),
+      .aluresultE(aluresultE),
+      .Rd2E(Rd2E),
+      .RdE(RdE),
+      .logicwriteM(logicwriteM),
+      .resultsrcM(resultsrcM),
+      .memwriteM(memwriteM),
+      .aluresultM(aluresultM),
+      .Rd2M(Rd2M),
+      .RdM(RdM)
+  );
+
+endmodule
