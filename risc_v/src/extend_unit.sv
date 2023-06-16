d633ddd37ec75fa95f4dbcc99dacdabf4dcdb12a
WalidAkash <walidakash070@gmail.com>
2023-06-16 12:58:45 +0600

    extend_unit added

--- risc_v/src/extend_unit.sv
+++ risc_v/src/extend_unit.sv
@@ -3,11 +3,21 @@
 // Description : This module shifts the instruction by an appropriate amount
 
 module extend_unit (
-    input  logic [31:7] instr,
-    input  logic [ 1:0] immsrc,
+    input logic [31:7] instr,
+    input logic immsrc,
     output logic [31:0] immext
 );
 
+  //-PROCEDURALS
 
+  always_comb begin : extend
+    if (!immsrc) begin  // R-type, I-type ALU, I-type Load
+      immext = {{20{instr[31]}}, instr[31:20]};
+    end else if (immsrc) begin  // S-type
+      immext = {{20{instr[31]}}, instr[31:25], instr[11:7]};
+    end else begin  // Undefined
+      immext = 32'bx;
+    end
+  end
 
 endmodule
