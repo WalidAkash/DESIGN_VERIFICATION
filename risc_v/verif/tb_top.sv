86e8fc84c6b39ad4a32e0130d6cc1da04b85f4cd
WalidAkash <walidakash070@gmail.com>
2023-06-19 10:15:56 +0600

    tb_top added

--- risc_v/verif/tb_top.sv
+++ risc_v/verif/tb_top.sv
@@ -4,12 +4,16 @@
 
 module tb_top;
   import rv32i_pkg::*;
+  `include "../include/tb_ess.sv"
 
   //-LOCALPARAMS
 
   localparam int ADW = 5;
 
   //-SIGNALS
+
+  // generates static task start_clk_i with tHigh:3 tLow:7
+  `CREATE_CLK(clk, 2, 2)
 
   logic [DPW-1:0] instrD;
   logic regwriteM;
@@ -33,6 +37,44 @@ module tb_top;
       .Rd2M(Rd2M),
       .RdM(RdM)
   );
+
+
+  //-PROCEDURALS
+
+  initial begin
+    start_clk();
+
+    for (int i = 0; i < 10; i++) begin
+      std::randomize(
+          instrD[6:0]
+      ) with {
+        instrD[6:0] inside {3, 19, 35, 51};
+      };
+      std::randomize(
+          instrD[14:12]
+      ) with {
+        instrD[14:12] inside {0, 1, 4, 5, 6, 7};
+      };
+      std::randomize(
+          instrD[30]
+      ) with {
+        instrD[30] inside {0, 1};
+      };
+
+      instrD[19:15] <= $urandom_range(0, 10);
+      instrD[24:20] <= $urandom_range(11, 20);
+
+      instrD[11:7] <= $urandom_range(0, 20);
+      instrD[29:25] <= $urandom_range(0, 20);
+      instrD[31] <= $urandom_range(0, 20);
+
+      repeat (10) @(posedge clk);
+
+    end
+
+    $finish;
+  end
+
 
 
 endmodule
