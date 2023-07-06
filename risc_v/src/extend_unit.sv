923ba85772149e44f27f2ec71ead79e92154f9f7
WalidAkash <walidakash070@gmail.com>
2023-07-06 11:40:10 +0600

    Updated extend_unit

--- risc_v/src/extend_unit.sv
+++ risc_v/src/extend_unit.sv
@@ -6,20 +6,30 @@ module extend_unit
   import rv32i_pkg::*;
 (
     input logic [DPW-1:7] instr_ext,
-    input logic immsrcD,
+    input logic [1:0] immsrcD,
     output logic [DPW-1:0] immextD
 );
 
   //-PROCEDURALS
 
   always_comb begin : extend
-    if (!immsrcD) begin  // R-type, I-type ALU, I-type Load
-      immextD = {{20{instr_ext[31]}}, instr_ext[31:20]};
-    end else if (immsrcD) begin  // S-type
-      immextD = {{20{instr_ext[31]}}, instr_ext[31:25], instr_ext[11:7]};
-    end else begin  // Undefined
-      immextD = 32'hx;
-    end
+    case (immsrcD)
+      2'b00: begin  // R-type, I-type ALU, I-type Load
+        immextD = {{20{instr_ext[31]}}, instr_ext[31:20]};
+      end
+
+      2'b01: begin  //S-type
+        immextD = {{20{instr_ext[31]}}, instr_ext[31:25], instr_ext[11:7]};
+      end
+
+      2'b10: begin  // B-type
+        immextD = {{20{instr_ext[31]}}, instr_ext[7], instr_ext[30:25], instr_ext[11:8], 1'b0};
+      end
+
+      default: begin  // Undefined
+        immextD = 32'hx;
+      end
+    endcase
   end
 
 endmodule
