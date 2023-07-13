beb5f8e8ca20d8207eb8132e3c718998b413a270
WalidAkash <walidakash070@gmail.com>
2023-07-13 15:49:49 +0600

    Updated instruction set

--- risc_v/verif/assembly_test/instrction_set.s
+++ risc_v/verif/assembly_test/instrction_set.s
@@ -1,4 +1,4 @@
-lw x3, 0(x3)
-lw x4, 1(x4)
-add x5, x4, x3
-sw x5, 4(x0)
+lw x4, 0(x4)          # Load memory address = [x4 + 0(Offset)] 's value in register x4
+lw x5, 4(x5)          # Load memory address = [x5 + 4(Offset)] 's value in register x5
+add x6, x5, x4        # Add x5 and x4 register values and store it into x6 register
+sw x6, 8(x0)          # store register x6's value in memory address = [x0 + 8(Offset)]
